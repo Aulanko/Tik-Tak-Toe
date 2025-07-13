1f76cea42800b5532b2aa81bed75d20028e7ca38
pronom8 <mangros19@gmail.com>
2025-07-13 15:03:02 +0300

    Cleaned up some boilerplate code & made basic turn change functionality for the Tik-Tak-Toe game

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [turn, setTurn] = useState('X')
+
+  
+
+  const handleClick = (e) =>{
+    if(e.target.innerText){
+      return;
+    }
+
+    e.target.innerText=turn
+
+    if(turn=='X'){
+      setTurn('O')
+    }
+    if(turn=='O'){
+      setTurn('X')
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Tik-Tak-Toe</h1>
+        <div className='board'>
+          <button id="1" onClick={handleClick} className='cell'></button>
+          <button id="2" onClick={handleClick} className='cell'></button>
+          <button id="3" onClick={handleClick} className='cell'></button>
+
+          <button id="4" onClick={handleClick} className='cell'></button>
+          <button id="5" onClick={handleClick} className='cell'></button>
+          <button id="6" onClick={handleClick} className='cell'></button>
+
+          <button id="7" onClick={handleClick} className='cell'></button>
+          <button id="8" onClick={handleClick} className='cell'></button>
+          <button id="9" onClick={handleClick} className='cell'></button>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
