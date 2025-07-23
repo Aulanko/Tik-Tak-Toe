477f0cdae3ce1a03dc78b34922156959da853b08
pronom8 <mangros19@gmail.com>
2025-07-23 11:40:27 +0300

    added a test that checks the initial game state has X as first player

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest' // not needed if globals:true
 import App from '../App'
+import { redirect } from 'happy-dom/lib/PropertySymbol.js'
 
 test('renders title', () => {
   render(<App />)
   expect(screen.getByText('Tik-Tac-Toe!')).toBeInTheDocument()
+})
+
+test('Initial game state has X as first player', () =>{
+  render(<App />)
+  expect(screen.getByText('Next player turn: X')).toBeInTheDocument()
+  const cells = screen.getAllByRole('button', {name: ''})
+  expect(cells).toHaveLength(9)
 })
--- vitest.config.js
+++ vitest.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'happy-dom', // lighter than jsdom
+    environment: 'happy-dom', 
     setupFiles: './vitest.setup.js',
-    globals: true, // so we don't need to import `describe`, `test`, etc.
+    globals: true, 
   }
 })
