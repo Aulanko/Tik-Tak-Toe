682c13bbe6e301c745b13f88aa717251116c3ffa
pronom8 <mangros19@gmail.com>
2025-07-24 19:46:19 +0300

    Added a test that checks if the cell turns into X when clicked on first turn, and can not be clicked again.

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest' // not needed if globals:true
+import userEvent from '@testing-library/user-event' 
 import App from '../App'
 import { redirect } from 'happy-dom/lib/PropertySymbol.js'
 
@@ -13,4 +14,18 @@ test('Initial game state has X as first player', () =>{
   expect(screen.getByText('Next player turn: X')).toBeInTheDocument()
   const cells = screen.getAllByRole('button', {name: ''})
   expect(cells).toHaveLength(9)
+})
+
+test('The cell turns into X when clicked on first turn, and can not be clicked again', async()=>{
+  render(<App />)
+  const cells = screen.getAllByRole('button')
+  const firstCell = cells[0]
+  
+  await userEvent.click(firstCell)
+  expect(firstCell).toHaveTextContent('X')
+  expect(screen.getByText('Next player turn: O')).toBeInTheDocument();
+  await userEvent.click(firstCell)
+  expect(firstCell).toHaveTextContent('X')
+  expect(screen.getByText('Next player turn: O')).toBeInTheDocument();
+
 })
